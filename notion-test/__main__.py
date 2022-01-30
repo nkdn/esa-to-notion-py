2a2fd992b88c324efe1592b3453cdb1391d11cc2
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-01-31 00:59:27 +0900

    文章や画像を追加する機構

--- notion-test/__main__.py
+++ notion-test/__main__.py
@@ -1,9 +1,22 @@
+from notion.block import (
+    HeaderBlock,
+    TextBlock,
+    ImageBlock
+)
 from notion.client import NotionClient
+import requests
 import settings
 
 def main():
-	client = NotionClient(token_v2=settings.TOKEN_V2)
-	page = client.get_block("https://www.notion.so/ce02ea0e970f4120b89efd39e5a354e2")
-	print(page.title)
+    client = NotionClient(token_v2=settings.NOTION_TOKEN_V2)
+    page = client.get_block("e2c3a61fb10a46f693db49b2b23f017a")
+    print(page)
+
+    page.children.add_new(HeaderBlock, title="はじめに")
+    text = "まず、`.env` ファイルをコピーしてください。\nそれが**とても大事** :exclamation: です。"
+    page.children.add_new(TextBlock, title=text)
+    image = page.children.add_new(ImageBlock)
+    image.upload_file_bin(requests.get("https://upload.wikimedia.org/wikipedia/commons/e/ef/StarfieldSimulation004.gif").content, "sample.gif", "image/gif")
+    print(image.source)
 
 main()
