a9296dc3c18c2e8c01ce4c90e4d5675a512e9e45
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-02-16 14:18:37 +0900

    再帰的に block を潜っていくようにした

--- esa-image-to-notion/__main__.py
+++ esa-image-to-notion/__main__.py
@@ -16,10 +16,18 @@ def main():
     print(children)
 
     for block in children:
+        reupload_image_recursively(block)
+
+# block 内にさらに block がある場合も再帰的に ImageBlock を探し、画像の Notion 用のアップロードをおこなう
+def reupload_image_recursively(block):
+    if len(block.children) == 0:
         if type(block) == ImageBlock:
             url = block.source
 
             if url.startswith("https://img.esa.io") or url.startswith("https://i.gyazo.com"):
                 wrapper.upload_image_by_url(image_block=block, url=url)
+    else:
+        for child_block in block.children:
+            reupload_image_recursively(child_block)
 
 main()
