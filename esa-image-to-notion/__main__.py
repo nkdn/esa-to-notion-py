2a3c3c403e520ab918e4932ccd14a336d9d2ca9b
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-02-16 15:08:39 +0900

    parent page id を指定して、その子記事を対象に処理するように

--- esa-image-to-notion/__main__.py
+++ esa-image-to-notion/__main__.py
@@ -11,20 +11,31 @@ def main():
     page_id = args[1]
 
     client = NotionClient(token_v2=settings.NOTION_TOKEN_V2)
-    page = client.get_block(page_id)
-    children = page.children
-    print(children)
+    child_page_ids = client.search_pages_with_parent(page_id)
+    esa_notion_mapping = dict()
 
-    for block in children:
-        reupload_image_recursively(block)
+    for page_id in child_page_ids:
+        page_id = page_id.replace("-", "")
+        page = client.get_block(page_id)
+        print("%s (%s)" % (page.title, page_id))
 
-# block 内にさらに block がある場合も再帰的に ImageBlock を探し、画像の Notion 用のアップロードをおこなう
+        children = page.children
+        esa_id = int(children[0].title.replace("ID: ", ""))
+        esa_notion_mapping[esa_id] = page_id
+
+        for block in children:
+            reupload_image_recursively(block)
+
+    print(esa_notion_mapping)
+
+# block 内にさらに block がある場合も再帰的に ImageBlock を探し、Notion 向けに画像アップロードをおこなう
 def reupload_image_recursively(block):
     if len(block.children) == 0:
         if type(block) == ImageBlock:
             url = block.source
 
             if url.startswith("https://img.esa.io") or url.startswith("https://i.gyazo.com"):
+                print("Re-uploading %s ..." % url)
                 wrapper.upload_image_by_url(image_block=block, url=url)
     else:
         for child_block in block.children:
