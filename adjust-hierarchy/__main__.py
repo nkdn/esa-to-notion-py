e691b2b3d37bef3dc07feebaa6fbcf288acd6b45
ハトネコエ hatonekoe@gmail.com <hato.nekoe@gmail.com>
2022-02-16 22:37:19 +0900

    adjust-hierarchy の機能がいったん完成

--- adjust-hierarchy/__main__.py
+++ adjust-hierarchy/__main__.py
@@ -1,11 +1,11 @@
 from notion.block import (
-    BasicBlock,
     PageBlock
 )
 from notion.client import NotionClient
-import re
 import settings
 import sys
+
+MAX_LAYER = 3
 
 def main():
     args = sys.argv
@@ -13,30 +13,41 @@ def main():
 
     client = NotionClient(token_v2=settings.NOTION_TOKEN_V2)
     parent_page = client.get_block(parent_page_id)
+    add_root_and_move(parent_page)
+
+def add_root_and_move(parent_page, layer=1):
     children = parent_page.children
+    root_blocks = []
 
-    last_block = children[-1]
+    for block in children:
+        if type(block) != PageBlock: continue
 
-    new_block = children.add_new(PageBlock, title="test")
-    last_block.move_to(new_block)
-    return
+        page_title = block.title
+        splitted_title = page_title.split("::")
+        if len(splitted_title) == 1: continue
+        root_title = splitted_title[0]
 
-    child_pages = []
+        if layer <= MAX_LAYER:
+            new_page_title = "::".join(splitted_title[1:])
+        else:
+            new_page_title = "/".join(splitted_title[1:])
 
-    for block in parent_page.children:
-        if type(block) == PageBlock:
-            child_pages.append({"id": block.id, "title": block.title})
+        same_title_blocks = list(
+            filter(lambda root_block: root_block.title == root_title, root_blocks))
+        if len(same_title_blocks):
+            exists_root_block = same_title_blocks[0]
+            block.move_to(exists_root_block)
+            block.title = new_page_title
+        else:
+            root_block = children.add_new(PageBlock, title=root_title)
+            root_blocks.append(root_block)
+            print("Added '%s' page" % root_title, flush=True)
+            block.move_to(root_block)
+            block.title = new_page_title
+        print(new_page_title, flush=True)
 
-    print(child_pages, flush=True)
-    return
-
-    for child_page in child_pages:
-        page_id = child_page["id"]
-        page_id = page_id.replace("-", "")
-        page = client.get_block(page_id)
-        print("%s (%s)" % (page.title, page_id))
-
-        for block in page.children:
-            change_url_recursively(block, esa_notion_mapping)
+    if layer <= MAX_LAYER:
+        for root_block in root_blocks:
+            add_root_and_move(root_block, layer=layer + 1)
 
 main()
